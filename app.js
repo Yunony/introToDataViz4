b5a6ea72a1c6d2e01dedc70f1412cd548f0191c3
Yuliya <yuliya.baran@gmail.com>
2013-04-25 19:44:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+$.getJSON('http://data.seattle.gov/api/views/3k2p-39jp/rows.json?jsonp=callback&max_rows=25', function(results)) {
+    
+    $.each.(result.data,function(i, value)  {
+        //console.log{results['data'][i][16]});
+        
+        //address
+        console.log(value[16])
+        
+        //time
+        console.log(value[15])
+        
+        //discription
+        console.log(value[12])
+        
+        //latitude, longitude
+        console.log(value[21])
+        console.log(value[20])
+        
+        L.marker([value[21], value[20]]).addTo(map)
+        .bindPopup('<h3>'+ value[12] + '<h3>' + value[16]+ '<br>' + value[15]);
+        
+        });
+    });
+    
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('http://{s}.tile.cloudmade.com/61d2e95f748e4d1da82b12d461f5174e/997/256/{z}/{x}/{y}.png', {
